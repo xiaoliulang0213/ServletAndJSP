36293f5f4863e9ff450674a8a4620f1f2b47e396
chengpunan <1783528055@qq.com>
2018-09-17 15:45:54 +0800

    添加了过滤器的测试类

---
+++ src/com/liuxiaonian/filter/FilterTest.java
@@ -0,0 +1,22 @@
+package com.liuxiaonian.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "FilterTest",urlPatterns = "/*")
+public class FilterTest implements Filter{
+    private FilterConfig filterConfig;
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        long begin = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest,servletResponse);
+        filterConfig.getServletContext().log("Ê±¼ä²îÎª:"+(System.currentTimeMillis()-begin)+"milliseconds");
+    }
+    public void destroy() {
+
+    }
+}
